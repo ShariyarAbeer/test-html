846b14bbe2f5df02187147eced2087997017279c
ShariyarAbeer <007abeershariyar@gmail.com>
2021-08-07 04:38:00 +0600

    update

---
+++ module-16-20-JS/if-else.js
@@ -0,0 +1,26 @@
+// job = false;
+
+// if(job){
+//     console.log("job ache")
+// }else{
+//     console.log("job nai")
+// }
+
+function verify(number){
+
+    if(number%2==0){
+        console.log("even number vai")
+          return "even number"
+
+    }else{
+      
+        console.log("odd number vai")
+          return "odd number"
+
+    }
+    
+  
+}
+
+let num = verify(44);
+console.log(num)
---
+++ module-16-20-JS/object.js
@@ -0,0 +1,23 @@
+// let car = {
+//     model:125,
+//     id:457,
+//     name:"car ami",
+//     price:"100 taka",
+//     isFourWheel: true,
+//     isSixDoor:false,
+// }
+// car.model = 78;
+// console.log(car)
+// console.log(car.isSixDoor)
+
+// print 10 even number 
+
+// for(i=0;i<20;i=i+2){
+//     console.log(i)
+// }
+
+// // print 10 odd number 
+
+// for(i=1;i<20;i=i+2){
+//     console.log(i)
+// }
