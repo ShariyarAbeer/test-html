e3dba044c98ab4487055806ad247123681499d94
ShariyarAbeer <007abeershariyar@gmail.com>
2021-08-30 23:02:38 +0600

    done

--- covid19LiveUpdate/app.js
+++ covid19LiveUpdate/app.js
@@ -3,40 +3,85 @@ const showAllFood = async () => {
   const searchValue = searchData.value;
   searchData.value = ""
   // const url = `https://api.covid19api.com/live/country/bangladesh/status/confirmed`
-  const url = `https://api.covid19api.com/live/country/${searchValue.toLowerCase()}/status/confirmed`
+  //const url = `https://api.covid19api.com/total/country/${searchValue.toLowerCase()}/status/confirmed`
+  const url = `https://api.covid19api.com/total/country/${searchValue.toLowerCase()}`
   // const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
   // console.log(url);
   const res = await fetch(url);
   const data = await res.json();
   // console.log(data);
 
-  const searchDataAllCountry = data.pop();
-  console.log(searchDataAllCountry);
-  console.log(searchDataAllCountry.Deaths);
-  const section = document.getElementById("all-country");
-  section.textContent = ""
 
-  // searchDataAllCountry.forEach(country => {
-  //   console.log(country);
-  const div = document.createElement("div");
-  div.innerHTML = `
+  if (data.length === 0) {
+    // console.log("data");
+    const section = document.getElementById("all-country");
+    section.textContent = ""
+
+    // searchDataAllCountry.forEach(country => {
+    //   console.log(country);
+    const div = document.createElement("div");
+    div.classList.add("bg-color");
+    div.classList.add("rounded");
+    div.innerHTML = `
       <div class="text-center">
         <div class="card h-100">
           <div class="card-body">
-            <h1 class="card-title">${searchDataAllCountry.Country}</h1>
-            <h6 class="card-text">Country Code: ${searchDataAllCountry.CountryCode}</h6>
-            <h3 class="card-title">Active Case: ${searchDataAllCountry.Active}</h3>
-            <h3 class="card-title">Confirmed Case: ${searchDataAllCountry.Confirmed}</h3>
-            <h3 class="card-title">Deaths: ${searchDataAllCountry.Deaths}</h3>
-            <h3 class="card-title">Update Date: ${searchDataAllCountry.Date.slice(0, 10)}</h3>
-            
+            <h6 class="card-text">There is no country by this name "${searchValue}"</h6>
+            </div>
+        </div>
+      </div>
+        `
+    section.appendChild(div);
+
+  } else {
+    const searchDataAllCountry = data.pop();
+    // console.log(searchDataAllCountry);
+    // console.log(searchDataAllCountry.Deaths);
+    const section = document.getElementById("all-country");
+    section.textContent = ""
+
+    // searchDataAllCountry.forEach(country => {
+    //   console.log(country);
+    const div = document.createElement("div");
+    div.classList.add("bg-color");
+    div.classList.add("rounded");
+    div.innerHTML = `
+      <div class="text-center">
+        <div class="card h-100">
+          <div class="card-body">
+            <h1 class="card-title">${searchDataAllCountry.Country}</h1> <br>
+            <table class="table">
+        <tbody class="fw-bold">
+            <tr>
+                <td>Country</td>
+                <td>Total</td>
+            </tr>
+            <tr>
+                <td>Active Case:</td>
+                <td>${searchDataAllCountry.Active}</td>
+            </tr>
+            <tr>
+                <td>Confirmed Case:</td>
+                <td>${searchDataAllCountry.Confirmed}</td>
+            </tr>
+            <tr>
+                <td>Deaths:</td>
+                <td>${searchDataAllCountry.Deaths}</td>
+            </tr>
+            <tr>
+                <td>Update Date:</td>
+                <td>${searchDataAllCountry.Date.slice(0, 10)}</td>
+            </tr>
+        </tbody>
+    </table>
           </div>
         </div>
       </div>
         `
-  section.appendChild(div);
+    section.appendChild(div);
 
-  // })
+    // })
+  }
 }
 
 // const showMore = async (itemInfo) => {
